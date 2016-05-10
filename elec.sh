bbe5f5575ff8d1766d5279e84ee0c7026428a91f
jt1134 <jt1134@gmail.com>
2016-05-10 02:56:26 -0500

    cleanup

--- elec.sh
+++ elec.sh
@@ -18,7 +18,7 @@ BS_DEL_TOT=$(calc -p "$BS_PL_TOT"+"$BS_UNPL_TOT")
 HC_DEL_TOT=$(calc -p "$HC_PL_TOT"+"$HC_UNPL_TOT")
 DEL_WON_TOT=$(calc -p "$BS_DEL_TOT"+"$HC_DEL_TOT")
 
-# Delegate Totals
+# Complete Delegate Totals
 DEL_TOT=4765 # Pledged + Unpledged
 PL_TOT=4051  # Pledged
 UNPL_TOT=714 # Unpledged (THESE DON'T REALLY COUNT YET)
@@ -29,9 +29,9 @@ UNPL_REM=$(calc -p "$UNPL_TOT"-"$UNPL_WON_TOT")
 DEL_REM=$(calc -p "$PL_REM"+"$UNPL_REM")
 
 # Delegate Goals
-C_C=1985 # 49%+
-MAJ=2026 # 50%+
-NOM=2383 # 58.82%+
+C_C=1985 # 49%+ Pledged
+MAJ=2026 # 50%+ Pledged
+NOM=2383 # 50%+ Total Delegates
 
 # All Goals are unmet by default
 BS_GOAL=""
@@ -41,17 +41,15 @@ HC_GOAL=""
 echo "2016 PRESIDENTIAL PRIMARY ELECTION DELEGATE COUNT AND PREDICTIONS
 
 =================================================================================================
- AS OF MAY 8, 2016	| BERNIE SANDERS		| \$HILLARY CLINTON		| TOTAL	|
-------------------------|-------------------------------|-------------------------------|-------|
- TOTAL PLEDGED		| 1419				| 1706				| 3125	|
- % OF TOTAL PLEDGED	| 45.41%   			| 54.59%   			|	|
-------------------------|-------------------------------|-------------------------------|-------|
- CONTESTED CONVENTION	| 566 - 61.12% OF REMAINING PL	| 269 - 29.99% OF REMAINING PL	|	|
- MAJORITY VOTE		| 607 - 65.55% OF REMAINING PL	| 310 - 34.56% OF REMAINING PL	|	|
- NOMINATION		| 250 - 926 PL + 38 - 714 UNPL	| 667 - 74.36% OF REMAINING PL	|	|
-----------------------------------------------------------------------------------------|-------|
- PLEDGED DELEGATES REMAINING								| 926	|
-=================================================================================================
+ AS OF MAY 9, 2016	| BERNIE SANDERS	| \$HILLARY CLINTON	| TOTAL	| REMAINING	|
+------------------------|-----------------------|-----------------------|-------|---------------|
+ PLEDGED DELEGATES      | 1419		35.03%	| 1706		42.11%	| 3125	| 926	22.86%	|
+ UNPLEDGED DELEGATES    | 41		5.74%	| 503		70.45%	| 544	| 170	23.81%	|
+------------------------|-----------------------|-----------------------|-----------------------|
+ CONTESTED CONVENTION   | 566 - 61.12% PLEDGED	| 269 - 30.13% PLEDGED	|
+ MAJORITY VOTE          | 607 - 65.55% PLEDGED	| 320 - 34.56% PLEDGED	|
+ NOMINATION             | 964 - 38+ UNPLEDGED	| 667 - 73.11% PLEDGED	|
+=========================================================================
 "
 
 # Remaining Primaries
@@ -78,43 +76,42 @@ do
 
     # Total Delegates Won
     PL_WON_TOT=$(calc -p "$PL_WON_TOT"+"$ST_PL")
+    #UNPL_WON_TOT=$(calc -p "$UNPL_WON_TOT"+"$ST_UNPL"
     DEL_WON_TOT=$(calc -p "$DEL_WON_TOT"+"$ST_PL")
 
     # Determine Delegate Totals
     BS_PL_WON=$(calc -p "round("$ST_PL"*0."$BS_VT",0)")
-    BS_PL_TOT=$(calc -p "$BS_PL_WON"+"$BS_PL_TOT")
+    BS_PL_TOT=$(calc -p "$BS_PL_TOT"+"$BS_PL_WON")
     BS_PL_PCT=$(calc -p "round("$BS_PL_TOT"/"$PL_TOT"*100,2)")
-    BS_DEL_TOT=$(calc -p "$BS_PL_TOT"+"$BS_UNPL_TOT")
+    #BS_UNPL_WON=$(calc -p "round("$ST_UNPL"*0."$BS_VT",0)")
+    #BS_UNPL_TOT=$(calc -p "$BS_UNPL_TOT"+"$BS_UNPL_WON")
     BS_UNPL_PCT=$(calc -p "round("$BS_UNPL_TOT"/"$UNPL_TOT"*100,2)")
+    BS_DEL_TOT=$(calc -p "$BS_PL_TOT"+"$BS_UNPL_TOT")
 
     HC_PL_WON=$(calc -p "$ST_PL"-"$BS_PL_WON")
     HC_PL_TOT=$(calc -p "$HC_PL_WON"+"$HC_PL_TOT")
     HC_PL_PCT=$(calc -p "round("$HC_PL_TOT"/"$PL_TOT"*100,2)")
-    HC_DEL_TOT=$(calc -p "$HC_PL_TOT"+"$HC_UNPL_TOT")
+    #HC_UNPL_WON=$(calc -p "$ST_UNPL"-"$BS_UNPL_WON")
+    #HC_UNPL_TOT=$(calc -p "$HC_UNPL_TOT"+"$HC_UNPL_WON")
     HC_UNPL_PCT=$(calc -p "round("$HC_UNPL_TOT"/"$UNPL_TOT"*100,2)")
+    HC_DEL_TOT=$(calc -p "$HC_PL_TOT"+"$HC_UNPL_TOT")
 
     # Total Delegates Remaining
     PL_REM=$(calc -p "$PL_REM"-"$ST_PL")
-    PL_REM_PCT=$(calc -p "round("$PL_WON_TOT"/"$PL_TOT"*100,2)")
+    PL_REM_PCT=$(calc -p "round("$PL_REM"/"$PL_TOT"*100,2)")
     #UNPL_REM=$(calc -p "$UNPL_REM"-"$ST_UNPL")
-    UNPL_REM_PCT=$(calc -p "round("$UNPL_WON_TOT"/"$UNPL_TOT"*100,2)")
+    UNPL_REM_PCT=$(calc -p "round("$UNPL_REM"/"$UNPL_TOT"*100,2)")
     DEL_REM=$(calc -p "$DEL_REM"-"$ST_PL")
     DEL_REM_PCT=$(calc -p "round("$DEL_WON_TOT"/"$DEL_TOT"*100,2)")
 
     echo "================================================================================================="
-    echo " $ST : $EL_DAY	| BERNIE SANDERS		| \$HILLARY CLINTON		| TOTAL	|"
-    echo "------------------------|-------------------------------|-------------------------------|-------|"
-    echo " PLEDGED DELEGATES      | $BS_PL_WON				| $HC_PL_WON				| $ST_PL	|"
-    echo " % OF STATE VT          | $BS_VT%                           | $HC_VT%                           |	|"
-    echo "------------------------|-------------------------------|-------------------------------|-------|"
-    echo " TOTAL PLEDGED          | $BS_PL_TOT                          | $HC_PL_TOT                          | $PL_WON_TOT	|"
-    echo " % OF TOTAL PLEDGED     | $BS_PL_PCT%   			| $HC_PL_PCT%   			|	|"
-    echo " PLEDGED REMAINING      |                               |                               | $PL_REM	|"
-    echo "------------------------|-------------------------------|-------------------------------|-------|"
-    echo " TOTAL UNPLEDGED        | $BS_UNPL_TOT                            | $HC_UNPL_TOT                           | $UNPL_WON_TOT	|"
-    echo " % OF TOTAL UNPLEDGED   | $BS_UNPL_PCT%   			| $HC_UNPL_PCT%			|	|"
-    echo " UNPLEDGED REMAINING    |                               |                               | $UNPL_REM	|"
-    echo "------------------------|-------------------------------|-------------------------------|-------|"
+    echo " $ST : $EL_DAY	| BERNIE SANDERS	| \$HILLARY CLINTON	| TOTAL	| REMAINING	|"
+    echo "------------------------|-----------------------|-----------------------|-------|---------------|"
+    echo " DELEGATES WON          | $BS_PL_WON		$BS_VT%	| $HC_PL_WON		$HC_VT%	| $ST_PL	|		|"
+    echo "------------------------|-----------------------|-----------------------|-------|---------------|"
+    echo " PLEDGED DELEGATES      | $BS_PL_TOT		$BS_PL_PCT%	| $HC_PL_TOT		$HC_PL_PCT%	| $PL_WON_TOT	| $PL_REM	$PL_REM_PCT%	|"
+    echo " UNPLEDGED DELEGATES    | $BS_UNPL_TOT            $BS_UNPL_PCT%   | $HC_UNPL_TOT           $HC_UNPL_PCT%  | $UNPL_WON_TOT   | $UNPL_REM   $UNPL_REM_PCT%  |"
+    echo "------------------------|-----------------------|-----------------------|-----------------------|"
 
     # determine probability of brokered convention, majority, or nomination
     for XP in "$C_C" "$MAJ" "$NOM"
@@ -125,47 +122,43 @@ do
 
         for C in 'BS' 'HC'
         do
-            [ "$C" == "BS" ] && C_PL_TOT="$BS_PL_TOT" && C_UNPL_TOT="$BS_UNPL_TOT" && C_DEL_TOT="$BS_DEL_TOT"
-            [ "$C" == "HC" ] && C_PL_TOT="$HC_PL_TOT" && C_UNPL_TOT="$HC_UNPL_TOT" && C_DEL_TOT="$HC_DEL_TOT"
+            [ "$C" == "BS" ] && C_PL_TOT="$BS_PL_TOT" && C_DEL_TOT="$BS_DEL_TOT"
+            [ "$C" == "HC" ] && C_PL_TOT="$HC_PL_TOT" && C_DEL_TOT="$HC_DEL_TOT"
 
             if [ "$1" == "" ]; then
                 # assumes every unpledged can be flipped
                 PL_REQ=$(calc -p "$XP"-"$C_PL_TOT")
-                PCT_REQ=$(calc -p "round("$PL_REQ"/"$PL_REM"*100,2)")
                 DEL_MAX=$(calc -p "$C_PL_TOT"+"$PL_REM"+"$UNPL_TOT")
 
                 if [ "$C_PL_TOT" -ge "$XP" ]; then
-                    C_GOAL="ACHIEVED			|"
+                    C_GOAL="ACHIEVED		|"
                 elif [ "$DEL_MAX" -lt "$XP" ]; then
-                    C_GOAL="IMPOSSIBLE			|"
+                    C_GOAL="IMPOSSIBLE		|"
                 elif [ "$PL_REQ" -gt "$PL_REM" ]; then
-                    PL_MIN=$(calc -p "$PL_REQ"-"$UNPL_TOT")
-                    if [ "$PL_MIN" -lt 0 ]; then PL_MIN=0; fi
                     UNPL_MIN=$(calc -p "$PL_REQ"-"$PL_REM")
-                    UNPL_MAX=$(calc -p "$PL_REQ"-"$PL_MIN")
-                    if [ "$UNPL_MIN" -lt 0 ]; then UNPL_MIN=0; fi
-                    C_GOAL="$PL_MIN - $PL_REM PL + $UNPL_MIN - $UNPL_MAX UNPL	|"
+                    C_GOAL="$PL_REQ - $UNPL_MIN+ UNPLEDGED	|"
                 else
-                    C_GOAL="$PL_REQ - $PCT_REQ% OF REMAINING PL	|"
+                    PCT_REQ=$(calc -p "round("$PL_REQ"/"$PL_REM"*100,2)")
+                    C_GOAL="$PL_REQ - $PCT_REQ% PLEDGED	|"
                 fi
             else
                 # include unpledged counts
+                [ "$XP" == 1985 ] && XP=2335
+                [ "$XP" == 2026 ] && XP=2383
                 PL_REQ=$(calc -p "$XP"-"$C_PL_TOT")
-                PCT_REQ=$(calc -p "round("$PL_REQ"/"$PL_REM"*100,2)")
                 DEL_MAX=$(calc -p "$C_DEL_TOT"+"$DEL_REM")
 
                 if [ "$C_DEL_TOT" -ge "$XP" ]; then
-                    C_GOAL="ACHIEVED			|"
+                    C_GOAL="ACHIEVED		|"
                 elif [ "$DEL_MAX" -lt "$XP" ]; then
-                    C_GOAL="IMPOSSIBLE			|"
+                    C_GOAL="IMPOSSIBLE		|"
                 elif [ "$PL_REQ" -gt "$PL_REM" ]; then
                     DEL_MIN=$(calc -p "$XP"-"$C_DEL_TOT")
-                    if [ "$DEL_MIN" -lt 0 ]; then DEL_MIN=0; fi
                     UNPL_MIN=$(calc -p "$PL_REQ"-"$PL_REM")
-                    if [ "$UNPL_MIN" -lt 0 ]; then UNPL_MIN=0; fi
-                    C_GOAL="$DEL_MIN TOTAL - $UNPL_MIN MINIMUM UNPL	|"
+                    C_GOAL="$DEL_MIN - $UNPL_MIN+ UNPLEDGED	|"
                 else
-                    C_GOAL="$PL_REQ - $PCT_REQ% OF REMAINING PL	|"
+                    PCT_REQ=$(calc -p "round("$PL_REQ"/"$PL_REM"*100,2)")
+                    C_GOAL="$PL_REQ - $PCT_REQ% PLEDGED	|"
                 fi
             fi
 
@@ -173,14 +166,14 @@ do
             [ "$C" == "HC" ] && HC_GOAL="$C_GOAL"
         done
 
-        echo "$GOAL $BS_GOAL $HC_GOAL	|"
+        echo "$GOAL $BS_GOAL $HC_GOAL"
         
         # Reset Goals
         BS_GOAL=""
         HC_GOAL=""
     done
 
-    echo "================================================================================================="
+    echo "========================================================================="
     echo
 done
  
